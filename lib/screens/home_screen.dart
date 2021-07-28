921737a9d38c48bcfb651c9653a62ed550628c2f
ThiagoMax <thiagomaximiliano21@gmail.com>
2021-07-28 13:16:28 -0300

    Drawer, jumpToPage, CircularProgressIndicator, LinearGradient e criação das categorias

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/products_tab.dart';
+import 'package:loja_virtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   //const HomeScreen({ Key? key }) : super(key: key);
@@ -10,7 +12,21 @@ class HomeScreen extends StatelessWidget {
     return PageView(
       controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
-      children: <Widget>[HomeTab()],
+      children: <Widget>[
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+            appBar: AppBar(
+              title: Text("Produtos"),
+              centerTitle: true,
+            ),
+            drawer: CustomDrawer(_pageController),
+            body: ProductsTab()),
+        Container(color: Colors.yellow),
+        Container(color: Colors.green)
+      ],
     );
   }
 }
